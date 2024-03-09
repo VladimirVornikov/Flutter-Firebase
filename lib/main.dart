729209c6421c598e339b687cb22a583cb340ea80
VladimirVornikov <vornikov-vladimir@mail.ru>
2024-03-09 01:25:58 +0100

    09.03

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'screens/home_page.dart';
-import 'package:firebase_core/firebase_core.dart';
 
-Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+void main() {
   runApp(MyApp());
 }
 
